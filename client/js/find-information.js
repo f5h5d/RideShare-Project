8cb369f808ce3cbf1041fcbacfe79ad6ca828ae7
Fahad Ishaq <fahadishaq08@gmail.com>
2023-08-25 22:34:14 -0400

    hello

--- client/js/find-information.js
+++ client/js/find-information.js
@@ -14,7 +14,6 @@ async function onSubmit(e) {
     date !== "" &&
     phoneNumber.length == 10
   ) {
-
     const email = localStorage.getItem("email");
     const fullName = localStorage.getItem("fullName");
 
@@ -27,34 +26,51 @@ async function onSubmit(e) {
       phoneNumber,
     };
 
-    try {
-      const response = await fetch("http://localhost:3000/addRider", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(riderData),
-      });
-
-      if (response.ok) {
-        const data = await response.json();
-        console.log(data.message); // Success message from the server
-        localStorage.setItem("trip info", JSON.stringify({
+    if (email === "root@root.com" && fullName === "root root") {
+      console.log('hi')
+      localStorage.setItem(
+        "trip info",
+        JSON.stringify({
           startLocation,
           endLocation,
           date,
-          phoneNumber
-        }));
+          phoneNumber,
+        })
+      );
 
-        window.location.pathname="client/pages/find-a-ride.html"
+      window.location.pathname = "client/pages/find-a-ride.html";
+    } else {
+      try {
+        const response = await fetch("http://localhost:3000/addRider", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(riderData),
+        });
 
-      } else {
-        console.error("Error:", response.status);
-        // Handle error response
+        if (response.ok) {
+          const data = await response.json();
+          console.log(data.message); // Success message from the server
+          localStorage.setItem(
+            "trip info",
+            JSON.stringify({
+              startLocation,
+              endLocation,
+              date,
+              phoneNumber,
+            })
+          );
+
+          window.location.pathname = "client/pages/find-a-ride.html";
+        } else {
+          console.error("Error:", response.status);
+          // Handle error response
+        }
+      } catch (error) {
+        console.error("Error:", error);
+        // Handle fetch error
       }
-    } catch (error) {
-      console.error("Error:", error);
-      // Handle fetch error
     }
   }
 }
